a2bc75ebc3777ff21c3c1310993f0da768098bd9
Raudel Gómez Smith <gomezsmith.raudel@gmail.com>
2024-07-26 09:54:04 +0200

    navBar/Color and position

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { MainContentComponent } from './main-content/main-content.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "", component: MainContentComponent},
+  {path: "imprint", component: ImprintComponent},
+  {path: "legal-notice", component: LegalNoticeComponent}
+];
