620f4ba31b66c5315b6695f113081a4f1d4206fb
Raudel Gómez Smith <gomezsmith.raudel@gmail.com>
2024-07-27 10:26:22 +0200

    whyme

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { WhyMeComponent } from './why-me/why-me.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [],
+  imports: [HomeComponent, WhyMeComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
